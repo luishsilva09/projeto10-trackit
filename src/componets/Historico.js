9136389b0e8004efefefd5f1566ba174161d188e
luis <luishsilva09@gmail.com>
2022-05-27 17:15:10 -0300

    pagina historico

--- src/componets/Historico.js
+++ src/componets/Historico.js
@@ -1,11 +1,38 @@
 import Footer from "./Footer";
 import TopBar from "./TopBar";
+import styled from "styled-components";
 
 export default function Historico(){
     return(
         <>
         <TopBar/>
+        <Container>
+            <h1>Historico</h1>
+            <h2>Em breve você poderá ver o histórico dos seus hábitos aqui!</h2>
+        </Container>
         <Footer/>
         </>
     )
 }
+
+const Container = styled.div`
+    margin-top:90px;
+    background:#E5E5E5 ;
+    padding: 17px; 
+    margin-bottom: 90px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    h1{
+        width: 340px;
+        font-size: 23px;
+        color: #126ba5;
+        margin-bottom: 17px;
+    }
+    h2{
+        width: 340px;
+        font-size:18px;
+        color:#bababa;
+        margin-bottom: 28px;
+    }
+`
